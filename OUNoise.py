421a07aed3706edfcef3c3e777b06784d9bc9deb
Helmut Strey <Helmut.Strey@gmail.com>
2020-03-26 14:27:27 -0400

    first commit

---
+++ OUNoise.py
@@ -0,0 +1,116 @@
+# this program is going to estimate parameters from simulated datasets that originate from an OU process
+# with noise added.  The parameters of the simulation and the length of the simulation are set through
+# arguments
+import langevin
+import pandas as pd
+import numpy as np
+import argparse
+import pymc3 as pm
+import theano.tensor as tt
+
+class Ornstein_Uhlenbeck(pm.Continuous):
+    """
+    Ornstein-Uhlenbeck Process
+    Parameters
+    ----------
+    B : tensor
+        B > 0, B = exp(-(D/A)*delta_t)
+    A : tensor
+        A > 0, amplitude of fluctuation <x**2>=A
+    delta_t: scalar
+        delta_t > 0, time step
+    """
+    
+    def __init__(self, A=None, B=None,
+                 *args, **kwargs):
+        super(Ornstein_Uhlenbeck, self).__init__(*args, **kwargs)
+        self.A = A
+        self.B = B
+        self.mean = 0.
+    
+    def logp(self, x):
+        A = self.A
+        B = self.B
+        
+        x_im1 = x[:-1]
+        x_i = x[1:]
+        
+        ou_like = pm.Normal.dist(mu=x_im1*B, tau=1.0/A/(1-B**2)).logp(x_i)
+        return pm.Normal.dist(mu=0.0,tau=1.0/A).logp(x[0]) + tt.sum(ou_like)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--output', action='store', default="./test",
+                        help='output file name')
+    parser.add_argument('-a', '--amplitude', action='store', type=float, default=1.0,
+                        help='amplitude of OU')
+    parser.add_argument('-r', '--tau', action='store', type=float, default=1.0,
+                        help='relaxation time tau of OU')
+    parser.add_argument('-w', '--noise', action='store', type=float, default=1.0,
+                        help='power of the noise')
+    parser.add_argument('-t', '--timestep', action='store', type=float, default=0.1,
+                        help='timestep')
+    parser.add_argument('-n', '--length', action='store', type=int, default=2000,
+                        help='length of simulation in timesteps')
+    parser.add_argument('-s', '--samples', action='store', type=int, default=2000,
+                        help='MCMC samples per run')
+    parser.add_argument('-b', '--dtstart', action='store', type=float, default=0.01,
+                        help='delta t range start')
+    parser.add_argument('-e', '--dtend', action='store', type=float, default=2.0,
+                        help='delta t range end')
+    parser.add_argument('-l', '--dtlength', action='store', type=int, default=50,
+                        help='delta t range number of points')
+
+    
+    # get arguments
+    arg = parser.parse_args()
+    
+    filename = arg.output
+    a = arg.amplitude
+    tau = arg.tau
+    pn = arg.noise
+    delta_t = arg.timestep
+    n = arg.length
+    mc_samples = arg.samples
+    dtstart = arg.dtstart
+    dtend = arg.dtend
+    dtlength = arg.dtlength
+    
+    delta_t_list = np.linspace(dtstart,dtend,dtlength)
+
+    result_array = None
+    for delta_t in delta_t_list:
+        print(delta_t)
+        data = langevin.time_series(A=a, D=a/tau, delta_t=delta_t, N=n)
+        dataN = data + np.random.normal(loc=0.0, scale=np.sqrt(pn), size=n)
+        with pm.Model() as model:
+            B = pm.Beta('B', alpha=5.0,beta=1.0)
+            A = pm.Uniform('A', lower=0, upper=5)
+            sigma = pm.Uniform('sigma',lower=0,upper=5)
+            
+            path = Ornstein_Uhlenbeck('path',A=A, B=B,shape=len(dataN))
+            dataObs = pm.Normal('dataObs',mu=path,sigma=sigma,observed=dataN)
+            trace = pm.sample(mc_samples,cores=4)
+        
+        a_mean = trace['A'].mean()
+        b_mean = trace['B'].mean()
+        a_std = trace['A'].std()
+        b_std = trace['B'].std()
+        sigma_mean = trace['sigma'].mean()
+        sigma_std = trace['sigma'].std()
+        avgpath = np.mean(trace['path'],axis=0)
+        stddiff = np.std(data-avgpath)
+        stdpath = np.std(trace['path'],axis=0).mean()
+        
+        results = [delta_t,a_mean,a_std,b_mean,b_std,sigma_mean,sigma_std,stddiff,stdpath]
+        if result_array is None:
+            result_array = results
+        else:
+            result_array = np.vstack((result_array, results))
+    
+    column_names = ["delta_t","a_mean","a_std","b_mean","b_std","sigma_mean","sigma_std","stddiff","stdpath"]
+    df=pd.DataFrame(result_array,columns=column_names)
+    df.to_csv(filename+'.csv',index=False)
+
+if __name__ == "__main__":
+    main()
---
+++ langevin.py
@@ -0,0 +1,70 @@
+import numpy as np
+from itertools import accumulate
+
+def time_series_sim(k,ga,diff,delta_t,N,G):
+    """ returns a time series that is the solution of a Langevin equation describing a Brownian particle in a harmonic potential
+    :param k: Spring constant
+    :param ga: friction coefficient
+    :param diff: Diffusion coefficient
+    :param delta_t: time step
+    :param N: number of samples that are returned
+    :param G: ratio of simulated data points and returned samples (100 means that only every 100th point is used)
+    :return:
+    """
+    # differential equation x_i = x_(i-1) - k/gamma*x_(i-1) + sqrt(2*D*delta_t)*w_i
+    def next_point(x, y):
+        amplitude = np.sqrt(2 * diff * delta_t / float(G))
+        return x - k / ga * x * delta_t / float(G) + amplitude * y
+
+    # random force
+    w=np.random.normal(0,1,N*G)
+    xx = np.fromiter(accumulate(w, next_point), np.float)
+
+    return xx[::G]  # only use every G point
+
+def time_series(A,D,delta_t,N):
+    """ returns a time series that is the solution of a Langevin equation describing a Brownian particle in a harmonic potential
+    :param A: mean square amplitude - oscillator strength
+    :param D: Diffusion coefficient
+    :param delta_t: time step
+    :param N: number of samples that are returned
+    :return:
+    """
+    # using Smolukowski solution for simulation
+
+    #first point
+    x=[np.random.normal(0,np.sqrt(A))]
+    stddev = np.sqrt(A * (1.0 - np.exp(-2.0 * D / A * delta_t)))
+
+    for i in range(N-1):
+        x.append(np.random.normal(x[-1]*np.exp(-D/A*delta_t),stddev))
+
+    return np.array(x)
+
+def time_series2(A,gamma,delta_t,N):
+    """ returns a time series that is the solution of a Langevin equation describing a Brownian particle in a harmonic potential
+    :param A: mean square amplitude - oscillator strength
+    :param gamma: friction coefficient
+    :param delta_t: time step
+    :param N: number of samples that are returned
+    :return:
+    """
+    # using Smolukowski solution for simulation
+
+    #first point
+    x=[np.random.normal(0,np.sqrt(A))]
+    stddev = np.sqrt(A * (1.0 - np.exp(-2.0 * gamma * delta_t)))
+
+    for i in range(N-1):
+        x.append(np.random.normal(x[-1]*np.exp(-gamma*delta_t),stddev))
+
+    return np.array(x)
+
+# differential equation x_i = x_(i-1) - k/gamma*x_(i-1) + sqrt(2*D*delta_t)*w_i
+# using 4th-order Runge-Kutta
+def next_point_RK4(x,y):
+    k0=-a*x
+    k1=-a*(x+k0/2.0)
+    k2=-a*(x+k1/2.0)
+    k3=-a*(x+k2)
+    return x + (k0+2*k1+2*k2+k3)/6 + ampl*y
