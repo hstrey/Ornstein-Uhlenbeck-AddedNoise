a704af3a3937030c2775812baa79bcb6450a98c5
Helmut Strey <Helmut.Strey@gmail.com>
2020-06-15 15:47:27 -0400

    preparing for running big MCMC simulation 7

--- OUNoiseMT.py
+++ OUNoiseMT.py
@@ -90,7 +90,7 @@ def main():
                           's':sigma_mean,
                           'ds':sigma_std}
         print(result_dict)
-        result_df.append(result_dict,ignore_index=True)
+        result_df = result_df.append(result_dict,ignore_index=True)
     print(result_df)                      
     result_df.to_csv(outfilename+'.csv',index=False)
 
